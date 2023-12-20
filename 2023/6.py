7929addb38146f2982f6c0e941bbbae956d575a0
punoko <36707250+punoko@users.noreply.github.com>
2023-12-20 15:56:59 +0100

    improve day 6

--- 2023/6.py
+++ 2023/6.py
@@ -1,18 +1,20 @@
 INPUT = open("input/6.txt").read().strip()
 TIME, DISTANCE = [line.split(":")[1].split() for line in INPUT.splitlines()]
-p1_input = [(int(TIME[i]), int(DISTANCE[i])) for i in range(len(TIME))]
-p2_input = [(int("".join(TIME)), int("".join(DISTANCE)))]
-for input in [p1_input, p2_input]:
-    output = []
-    score = 1
-    for time, distance in input:
-        button_min = 0
-        while button_min * (time - button_min) <= distance:
-            button_min += 1
-        button_max = time
-        while button_max * (time - button_max) <= distance:
-            button_max -= 1
-        output.append(len(range(button_min, button_max + 1)))
-    for num in output:
-        score *= num
-    print(score)
+
+
+def func(time, distance):
+    start = 0
+    while start * (time - start) <= distance:
+        start += 1
+    end = time
+    while end * (time - end) <= distance:
+        end -= 1
+    return len(range(start, end + 1))
+
+
+part1 = 1
+for i in range(len(TIME)):
+    part1 *= func(int(TIME[i]), int(DISTANCE[i]))
+part2 = func(int("".join(TIME)), int("".join(DISTANCE)))
+print(part1)
+print(part2)
