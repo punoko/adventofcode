c9dda998e18197b53a5c6d725dd994a762af00a1
punoko <36707250+punoko@users.noreply.github.com>
2025-05-08 05:12:34 +0200

    2024 day 7 part 2

--- 2024/7.py
+++ 2024/7.py
@@ -8,18 +8,28 @@ part1 = 0
 part2 = 0
 input = Path("input", Path(__file__).name).with_suffix(".txt")
 for line in input.read_text().splitlines():
-    value, numbers = line.split(":")
-    value = int(value)
-    numbers = tuple(int(x) for x in numbers.split())
-    for operators in range(2 ** (len(numbers) - 1)):
-        result = numbers[0]
-        for i, num in enumerate(numbers[1:]):
-            if operators & 1 << i:
-                result = result * num
-            else:
-                result = result + num
+    numbers = [int(n) for n in line.replace(":", "").split()]
+    value = numbers.pop(0)
+    first = numbers.pop(0)
+    found = False
+    for seed in range(3 ** len(numbers)):
+        result = first
+        concat = False
+        for i, num in enumerate(numbers):
+            operator = (seed // (3**i)) % 3
+            if operator == 0:
+                result += num
+            elif operator == 1:
+                result *= num
+            elif operator == 2:
+                result = int(str(result) + str(num))
+                concat = True
         if result == value:
-            part1 += result
-            break
+            found = True
+            if not concat:
+                part1 += value
+                break
+    if found:
+        part2 += value
 print(part1)
 print(part2)
