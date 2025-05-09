dccdf4ee9819699be0e8b80f3590e29aa995deba
punoko <36707250+punoko@users.noreply.github.com>
2025-05-10 00:22:27 +0200

    2024 day 8 part 2

--- 2024/8.py
+++ 2024/8.py
@@ -2,29 +2,38 @@
 # requires-python = ">=3.13"
 # dependencies = []
 # ///
-from pathlib import Path
 from collections import defaultdict
+from math import gcd
+from pathlib import Path
 
+part1 = set()
+part2 = set()
 input = Path("input", Path(__file__).name).with_suffix(".txt")
 GRID = input.read_text().splitlines()
-antennas = defaultdict(list)
+ANTENNAS = defaultdict(list)
 for y, row in enumerate(GRID):
     for x, frequency in enumerate(row):
         if frequency != ".":
-            antennas[frequency].append((x, y))
-antinodes = set()
-for frequency in antennas:
-    tmp = antennas[frequency]
-    while len(tmp) > 1:
-        ax, ay = tmp.pop()
-        for bx, by in tmp:
-            for x, y in [
-                (2 * ax - bx, 2 * ay - by),
-                (2 * bx - ax, 2 * by - ay),
-            ]:
-                if 0 <= x < len(GRID[0]) and 0 <= y < len(GRID):
-                    antinodes.add((x, y))
-part1 = len(antinodes)
-part2 = 0
-print(part1)
-print(part2)
+            ANTENNAS[frequency].append((x, y))
+for antennas in ANTENNAS.values():
+    while len(antennas) > 1:
+        a_x, a_y = antennas.pop()
+        for b_x, b_y in antennas:
+            v_x = (a_x - b_x) // gcd(a_x - b_x, a_y - b_y)
+            v_y = (a_y - b_y) // gcd(a_x - b_x, a_y - b_y)
+            x, y = a_x, a_y
+            while 0 <= x < len(GRID[0]) and 0 <= y < len(GRID):
+                if x == 2 * a_x - b_x:
+                    part1.add((x, y))
+                part2.add((x, y))
+                x += v_x
+                y += v_y
+            x, y = a_x, a_y
+            while 0 <= x < len(GRID[0]) and 0 <= y < len(GRID):
+                if x == 2 * b_x - a_x:
+                    part1.add((x, y))
+                part2.add((x, y))
+                x -= v_x
+                y -= v_y
+print(len(part1))
+print(len(part2))
