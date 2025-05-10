042a247eb901101847f04bb919a7b12ba8d0c9a6
punoko <36707250+punoko@users.noreply.github.com>
2025-05-10 17:23:10 +0200

    2024 day 9 part 2

--- 2024/9.py
+++ 2024/9.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 def checksum(blocks: list[str]) -> int:
     sum = 0
-    for pos, id in enumerate(blocks):
+    for position, id in enumerate(blocks):
         if id != ".":
-            sum += pos * int(id)
+            sum += position * int(id)
     return sum
 
 
@@ -17,25 +17,48 @@ input = Path("input", Path(__file__).name).with_suffix(".txt")
 disk = [int(length) for length in input.read_text().strip()]
 blocks = []
 for i, length in enumerate(disk):
-    if i % 2 == 0:  # file
+    if i % 2 == 0:  # files
         blocks.extend([str(i // 2)] * length)
     else:  # free space
         blocks.extend(["."] * length)
 
 part1 = blocks.copy()
 i = len(part1)
-try:
-    j = part1.index(".")
-except ValueError:
-    j = i
-while i > j:
+k = 0
+while i > 0:
     i -= 1
     if part1[i] == ".":
         continue
-    part1[j] = part1[i]
-    part1[i] = "."
     try:
-        j = part1.index(".", j + 1, i)
+        k = part1.index(".", k, i)
     except ValueError:
         break
+    part1[k] = part1[i]
+    part1[i] = "."
 print(checksum(part1))
+
+part2 = blocks.copy()
+i = len(part2)
+while i > 0:
+    j = i
+    i -= 1
+    if part2[i] == ".":
+        continue
+    while i > 0 and part2[i - 1] == part2[i]:
+        i -= 1
+    try:
+        k = part2.index(".", 0, i)
+    except ValueError:
+        break
+    length = j - i
+    free = ["."] * length
+    while k < i:
+        if part2[k : k + length] == free:
+            part2[k : k + length] = part2[i:j]
+            part2[i:j] = free
+            break
+        try:
+            k = part2.index(".", k + 1, i)
+        except ValueError:
+            break
+print(checksum(part2))
