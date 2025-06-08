d28fb221f76c7295b02918c1c911e4a91e2c39f7
punoko <36707250+punoko@users.noreply.github.com>
2025-06-09 00:08:48 +0200

    2024 day 11 part 2 (recursion!)

--- 2024/11.py
+++ 2024/11.py
@@ -4,22 +4,30 @@
 # ///
 from pathlib import Path
 
+def solve(stone: int, depth: int) -> int:
+    if (stone, depth) in SAVED:
+        return SAVED[(stone, depth)]
+    if depth == 0:
+        return 1
+    if stone == 0:
+        result = solve(1, depth - 1)
+    elif len(str(stone)) % 2 == 0:
+        string = str(stone)
+        half = len(string) // 2
+        result = solve(int(string[:half]), depth - 1)
+        result += solve(int(string[half:]), depth - 1)
+    else:
+        result = solve(stone * 2024, depth - 1)
+    SAVED[(stone, depth)] = result
+    return result
+
+
 part1 = 0
 part2 = 0
 input = Path("input", Path(__file__).name).with_suffix(".txt")
-# input = Path("input/test.txt")
-stones = input.read_text().split()
-for blink in range(25):
-    tmp = []
-    for stone in stones:
-        if stone == "0":
-            tmp.append("1")
-        elif not len(stone) % 2:
-            half = int(len(stone) / 2)
-            tmp.append(str(int(stone[:half])))
-            tmp.append(str(int(stone[half:])))
-        else:
-            tmp.append(str(int(stone) * 2024))
-    stones = tmp
-print(len(stones))
+SAVED = dict()
+for stone in input.read_text().split():
+    part1 += solve(int(stone), 25)
+    part2 += solve(int(stone), 75)
+print(part1)
 print(part2)
