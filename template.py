ec36665e0be2cde37509c072d259a35fb04578d0
punoko <36707250+punoko@users.noreply.github.com>
2025-12-01 12:33:47 +0100

    update template for 2025

--- template.py
+++ template.py
@@ -1,13 +1,19 @@
 # /// script
-# requires-python = ">=3.13"
+# requires-python = ">=3.14"
 # dependencies = []
 # ///
+
+import sys
 from pathlib import Path
 
 part1 = 0
 part2 = 0
+
 input = Path("input", Path(__file__).name).with_suffix(".txt")
+input = Path(sys.argv[1]) if len(sys.argv) > 1 else input
+
 for line in input.read_text().splitlines():
     pass
+
 print(part1)
 print(part2)
