aca491cf5083f5420b3886ea462014d0ec141bb5
punoko <36707250+punoko@users.noreply.github.com>
2025-12-03 08:36:39 +0100

    2025 day 3 make it cuter

--- 2025/3.py
+++ 2025/3.py
@@ -1,29 +1,25 @@
 import sys
 
+
+def maximum_joltage(bank: list[int], count: int) -> int:
+    joltage = 0
+    i = 0
+    for j in range(1 - count, 1):
+        slice = bank[i:j] if j < 0 else bank[i:]
+        battery = max(slice)
+        joltage = 10 * joltage + battery
+        i += slice.index(battery) + 1
+    return joltage
+
+
 input = sys.stdin.read()
 part1 = 0
 part2 = 0
 
 for line in input.splitlines():
-    bank = [int(x) for x in line]
-
-    battery = max(bank[:-1])
-    i = bank.index(battery) + 1
-    part1 += battery * 10
-    part1 += max(bank[i:])
-
-    joltage = 0
-    i = 0
-    for j in range(-11, 1):
-        joltage *= 10
-        if j:
-            slice = bank[i:j]
-        else:
-            slice = bank[i:]
-        battery = max(slice)
-        i += slice.index(battery) + 1
-        joltage += battery
-    part2 += joltage
+    bank = [int(n) for n in line]
+    part1 += maximum_joltage(bank, 2)
+    part2 += maximum_joltage(bank, 12)
 
 print(part1)
 print(part2)
