db499970b9770d459feadf37c1c3846270406841
punoko <36707250+punoko@users.noreply.github.com>
2025-12-03 08:26:24 +0100

    2025 day 3 part 2

--- 2025/3.py
+++ 2025/3.py
@@ -5,12 +5,25 @@ part1 = 0
 part2 = 0
 
 for line in input.splitlines():
-    batteries = [int(x) for x in line]
-    digit = max(batteries[:-1])
-    index = batteries.index(digit) + 1
-    part1 += digit * 10
-    digit = max(batteries[index:])
-    part1 += digit
+    bank = [int(x) for x in line]
+
+    battery = max(bank[:-1])
+    i = bank.index(battery) + 1
+    part1 += battery * 10
+    part1 += max(bank[i:])
+
+    joltage = 0
+    i = 0
+    for j in range(-11, 1):
+        joltage *= 10
+        if j:
+            slice = bank[i:j]
+        else:
+            slice = bank[i:]
+        battery = max(slice)
+        i += slice.index(battery) + 1
+        joltage += battery
+    part2 += joltage
 
 print(part1)
 print(part2)
