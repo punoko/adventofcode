177676dbbbcb8070163d764909049cc288a82b22
punoko <36707250+punoko@users.noreply.github.com>
2025-12-04 08:51:33 +0100

    2025 day 4 part 2

--- 2025/4.py
+++ 2025/4.py
@@ -1,23 +1,19 @@
 import sys
 
 
-def get(r: int, c: int) -> str:
+def get(map, r: int, c: int) -> str:
     if r < 0 or r >= ROW or c < 0 or c >= COL:
         return "!"
-    return MAP[r][c]
+    return map[r][c]
 
 
-def can_access_roll(row: int, col: int) -> bool:
-    print("checking", row, col)
+def can_access_roll(map, row: int, col: int) -> bool:
     neigh = 0
     for dir in DIRECTIONS:
         r = row + dir[0]
         c = col + dir[1]
-        bla = get(r, c)
-        print(bla, r, c)
-        if bla == "@":
+        if get(map, r, c) == "@":
             neigh += 1
-    print("total=", neigh)
     return neigh < MAX_ROLLS
 
 
@@ -25,18 +21,23 @@ input = sys.stdin.read()
 part1 = 0
 part2 = 0
 
-MAP = tuple(input.splitlines())
+MAP = tuple(tuple(r) for r in input.splitlines())
 ROW = len(MAP)
 COL = len(MAP[0])
 DIRECTIONS = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 MAX_ROLLS = 4
-for r, line in enumerate(MAP):
-    for c, char in enumerate(line):
-        if char != "@":
-            print("char", char, r, c, "skipping")
-            continue
-        print("char", char, r, c, "OK")
-        if can_access_roll(r, c):
-            part1 += 1
+map = [list(row) for row in MAP]
+while True:
+    changed = False
+    for r, line in enumerate(map):
+        for c, char in enumerate(line):
+            if char != "@":
+                continue
+            if can_access_roll(map, r, c):
+                changed = True
+                map[r][c] = "."
+                part2 += 1
+    if not changed:
+        break
 print(part1)
 print(part2)
