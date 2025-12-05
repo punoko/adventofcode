0a0a425b93a5c3d04b2e0f45d53d6c2d440b0853
punoko <36707250+punoko@users.noreply.github.com>
2025-12-05 23:42:00 +0100

    add script to download input

---
+++ get_input.py
@@ -0,0 +1,28 @@
+import argparse
+import datetime
+import os
+import sys
+
+import requests
+
+DECEMBER = 12
+tz = datetime.timezone(offset=datetime.timedelta(hours=-5))  # UTC-5
+today = datetime.datetime.now(tz=tz).date()
+year = today.year if today.month == DECEMBER else today.year - 1
+day = today.day if today.month == DECEMBER else None
+
+parser = argparse.ArgumentParser(description="Get AoC input")
+parser.add_argument("-y", "--year", type=int, default=year, help="default: %(default)s")
+parser.add_argument("-d", "--day", type=int, default=day, help="default: %(default)s")
+args = parser.parse_args()
+
+url = f"https://adventofcode.com/{args.year}/day/{args.day}/input"
+try:
+    cookies = {"session": os.environ["SESSION"]}
+except KeyError:
+    sys.exit("SESSION environment variable containing session cookie required")
+
+response = requests.get(url=url, cookies=cookies, timeout=3)
+print(args.year, "day", args.day, file=sys.stderr)
+print(response.text)
+response.raise_for_status()
