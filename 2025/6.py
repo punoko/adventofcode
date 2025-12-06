3cf6ddd55087dedcb5dc92e27da747f9303eccff
punoko <36707250+punoko@users.noreply.github.com>
2025-12-06 08:17:49 +0100

    2025 day 6 part 2

--- 2025/6.py
+++ 2025/6.py
@@ -13,11 +13,32 @@ COL = len(GRID[0])
 for c in range(COL):
     operator = GRID[-1][c]
     numbers = [int(GRID[x][c]) for x in range(ROW - 1)]
-    print(operator, numbers)
     if operator == "+":
         part1 += sum(numbers)
     else:
         part1 += math.prod(numbers)
+
+
+G = [line for line in input.split("\n") if line]
+ROW = len(G)
+COL = len(G[0])
+operator = ""
+numbers = []
+for c in range(COL):
+    if G[-1][c] != " ":
+        operator = G[-1][c]
+    string = [G[r][c] for r in range(ROW - 1)]
+    string = "".join(string).strip()
+    if string:
+        numbers.append(int(string))
+        if c < COL - 1:
+            continue
+    if operator == "+":
+        part2 += sum(numbers)
+    elif operator == "*":
+        part2 += math.prod(numbers)
+    numbers = []
+
 
 print(part1)
 print(part2)
