8f3c10baf997e996debe75ac032fb2a363fa1724
punoko <36707250+punoko@users.noreply.github.com>
2025-12-07 12:40:19 +0100

    2025 day 7 make it cuter

--- 2025/7.py
+++ 2025/7.py
@@ -1,23 +1,18 @@
 import sys
-from collections import defaultdict
 
 input = sys.stdin.read()
 part1 = 0
 
-# count timelines for each beam, start with 1
-GRID = input.splitlines()
-start = GRID.pop(0).index("S")
-beams: dict[int, int] = defaultdict(int)
-beams[start] = 1
-while GRID:
-    row = GRID.pop(0)
-    for i, space in enumerate(row):
-        if i in beams and space == "^":
+beams = [0] * input.index("\n")
+beams[input.index("S")] = 1
+for line in input.splitlines()[1:]:
+    for i, c in enumerate(line):
+        if c == "^" and beams[i]:
             part1 += 1
             beams[i - 1] += beams[i]
             beams[i + 1] += beams[i]
-            del beams[i]
-part2 = sum(beams.values())
+            beams[i] = 0
+part2 = sum(beams)
 
 print(part1)
 print(part2)
