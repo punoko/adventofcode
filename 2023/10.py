4d689769704fe80b99c3fbbc541a4656811ea2d9
punoko <36707250+punoko@users.noreply.github.com>
2026-01-11 05:42:35 +0100

    2023 day 10 part 2

--- 2023/10.py
+++ 2023/10.py
@@ -5,12 +5,12 @@ NORTH = (-1, 0)
 EAST = (0, 1)
 WEST = (0, -1)
 CONNECTIONS = {
-    "|": (NORTH, SOUTH),
-    "-": (EAST, WEST),
-    "L": (NORTH, EAST),
-    "J": (NORTH, WEST),
-    "7": (SOUTH, WEST),
-    "F": (SOUTH, EAST),
+    "|": {NORTH, SOUTH},
+    "-": {EAST, WEST},
+    "L": {NORTH, EAST},
+    "J": {NORTH, WEST},
+    "7": {SOUTH, WEST},
+    "F": {SOUTH, EAST},
 }
 
 input = sys.stdin.read()
@@ -23,18 +23,27 @@ LOOP = set()
 r, c = next((r, c) for r in range(R) for c in range(C) if G[r][c] == "S")
 LOOP.add((r, c))
 
-# pick first step
+# replace S with its actual pipe shape so we can solve part2
+S_connections = set()
 if G[r - 1][c] in ("|", "7", "F"):
-    r -= 1  # go north
-elif G[r + 1][c] in ("|", "L", "J"):
-    r += 1  # go south
-elif G[r][c + 1] in ("-", "J", "7"):
-    c += 1  # go east
-elif G[r][c - 1] in ("-", "L", "F"):
-    c -= 1  # go west
+    S_connections.add(NORTH)
+if G[r + 1][c] in ("|", "L", "J"):
+    S_connections.add(SOUTH)
+if G[r][c + 1] in ("-", "J", "7"):
+    S_connections.add(EAST)
+if G[r][c - 1] in ("-", "L", "F"):
+    S_connections.add(WEST)
+S = next(k for k, v in CONNECTIONS.items() if S_connections == v)
+G[r] = G[r].replace("S", S)
+
+# pick any of the two S connections as first step
+rr, cc = S_connections.pop()
+r += rr
+c += cc
 LOOP.add((r, c))
 
-while True:  # go through loop
+# go through loop
+while True:
     connections = [(r + rr, c + cc) for rr, cc in CONNECTIONS[G[r][c]]]
     try:
         r, c = next((r, c) for r, c in connections if (r, c) not in LOOP)
@@ -44,3 +53,22 @@ while True:  # go through loop
 
 part1 = len(LOOP) // 2
 print(part1)
+
+part2 = 0
+for r in range(R):
+    inside = False
+    entry = None
+    for c in range(C):
+        tile = G[r][c]
+        if (r, c) not in LOOP:
+            if inside:
+                part2 += 1
+        elif tile in ("F", "L"):
+            entry = tile
+        elif (
+            tile == "|"
+            or (entry == "L" and tile == "7")
+            or (entry == "F" and tile == "J")
+        ):
+            inside = not inside
+print(part2)
